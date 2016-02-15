8d5d70e3ff4a6a22528b25019e365588ec583587
Dave Voyles <dvoyles@microsoft.com>
2016-02-15 11:58:51 -0500

    Added text for infobox

--- public/js/map.js
+++ public/js/map.js
@@ -6,7 +6,6 @@ var Map = (function () {
         this.aCenterLoc = new Microsoft.Maps.Location(this.aDefaultLngLat[0], this.aDefaultLngLat[1]);
         this.infoboxOptions = { width: 400, height: 250, showCloseButton: true, visible: false };
         this.bingAPIKey = 'Ah1_aJohnC76ttqxM-PjSm5rsabmFcLSOujmuYvfSmKSHAOk9Xm2X2E6AtCQBNPk';
-        this.testVar = 'string';
         this.defaultInfobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(38.09024, -95.712891), this.infoboxOptions); // Put info box slight above pin
         this.myMap = new Microsoft.Maps.Map(document.getElementById('BingMap'), {
             credentials: this.bingAPIKey
@@ -14,6 +13,7 @@ var Map = (function () {
         this.pushpin = new Microsoft.Maps.Pushpin(this.aCenterLoc);
         // Center map on the United States   
         this.myMap.setView({ zoom: 5, center: this.aCenterLoc });
+        this.setInfoboxContent("Dave", "Philadelphia", "Games", "I am a person!", "http://www.twitter.com/DaveVoyles", "www.DaveVoyles.com", "https://www.google.com/imgres?imgurl=http://www.shawnee.edu/game-conference/img/David-Voyles.jpg&imgrefurl=http://www.shawnee.edu/game-conference/&h=320&w=320&tbnid=nbmS3Ix4lGQWeM:&docid=T2WMgVqAREbcIM&ei=vP_BVrKuIIWe-gGkrJaAAw&tbm=isch&ved=0ahUKEwiy_OmDm_rKAhUFjz4KHSSWBTAQMwgfKAIwAg");
         // Create pin and infobox
         this.myMap.entities.clear();
         this.myMap.entities.push(this.pushpin);
@@ -26,6 +26,26 @@ var Map = (function () {
     Map.prototype.pushPinClickHandler = function () {
         var _this = this;
         Microsoft.Maps.Events.addHandler(this.pushpin, 'click', function () { return _this.defaultInfobox.setOptions({ visible: true }); });
+    };
+    /**
+     * TODO: Consider changing spec to an array of strings
+     * TODO: Consdier changing return type to string
+     */
+    Map.prototype.setInfoboxContent = function (name, city, spec, bio, twitter, websiteUrl, img) {
+        this.defaultInfobox.setHtmlContent('<div class="bio-container">' +
+            '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>' +
+            '<h3>' + city + '</h3>' +
+            '<h3>' + spec + '</h3>' +
+            '<div id="bodyContent">' +
+            bio +
+            '</div> <!-- .bodyContent-->' +
+            '<p>' +
+            '<a href=' + twitter + '/>' + twitter +
+            '<p>' +
+            '<a href="' + websiteUrl + '"/>' + websiteUrl +
+            '</div> <!-- Bio Container -->' +
+            '<img src=' + img + ' class = "evangelist-img">' +
+            '</div><!-- .bio-container -->');
     };
     return Map;
 })();
--- public/js/map.ts
+++ public/js/map.ts
@@ -14,8 +14,9 @@ class Map {
     private infoboxOptions = { width: 400, height: 250, showCloseButton: true, visible: false };
     private bingAPIKey     = 'Ah1_aJohnC76ttqxM-PjSm5rsabmFcLSOujmuYvfSmKSHAOk9Xm2X2E6AtCQBNPk';       
     
-    private testVar = 'string';
+    
     constructor() {
+        
         this.defaultInfobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(38.09024, -95.712891), this.infoboxOptions); // Put info box slight above pin
         this.myMap          = new Microsoft.Maps.Map(document.getElementById('BingMap'), {
             credentials: this.bingAPIKey
@@ -24,6 +25,8 @@ class Map {
         
         // Center map on the United States   
         this.myMap.setView({ zoom: 5, center: this.aCenterLoc })
+        
+        this.setInfoboxContent("Dave", "Philadelphia", "Games", "I am a person!", "http://www.twitter.com/DaveVoyles", "www.DaveVoyles.com", "https://www.google.com/imgres?imgurl=http://www.shawnee.edu/game-conference/img/David-Voyles.jpg&imgrefurl=http://www.shawnee.edu/game-conference/&h=320&w=320&tbnid=nbmS3Ix4lGQWeM:&docid=T2WMgVqAREbcIM&ei=vP_BVrKuIIWe-gGkrJaAAw&tbm=isch&ved=0ahUKEwiy_OmDm_rKAhUFjz4KHSSWBTAQMwgfKAIwAg");
      
         // Create pin and infobox
         this.myMap.entities.clear();
@@ -33,12 +36,39 @@ class Map {
         this.pushPinClickHandler();
     }   
     
+    
     /** 
      * Displays infobox on screen when user clicks
      */
     private pushPinClickHandler () : any
-     {
-         Microsoft.Maps.Events.addHandler(this.pushpin, 'click', ()=> this.defaultInfobox.setOptions({visible: true}));  
+    {
+        Microsoft.Maps.Events.addHandler(this.pushpin, 'click', ()=> this.defaultInfobox.setOptions({visible: true}));  
     }
+    
+    
+    /**
+     * TODO: Consider changing spec to an array of strings
+     * TODO: Consdier changing return type to string
+     */
+    private setInfoboxContent (name: string, city: string, spec: string, bio: string, twitter: string, websiteUrl: string, img: string) : any 
+    {
+        this.defaultInfobox.setHtmlContent(
+            '<div class="bio-container">' +
+                '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>' +
+                '<h3>' + city + '</h3>' +
+                '<h3>' + spec + '</h3>' +
+                '<div id="bodyContent">' +
+                bio +
+                '</div> <!-- .bodyContent-->' +
+                '<p>' +
+                '<a href=' + twitter + '/>' + twitter +
+                '<p>' +
+                '<a href="' + websiteUrl + '"/>' + websiteUrl +
+                '</div> <!-- Bio Container -->' +
+                '<img src=' + img + ' class = "evangelist-img">' +
+            '</div><!-- .bio-container -->'
+        )
+    }
+    
              
 };
